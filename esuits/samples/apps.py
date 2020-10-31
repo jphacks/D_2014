40dfe0da2a664bac78a09ce1af1eb4efbcf48d3a
yuasa <yuasalily1011@gmail.com>
2020-11-01 01:22:30 +0900

    Made samples

---
+++ esuits/samples/apps.py
@@ -0,0 +1,4 @@
+from django.apps import AppConfig
+
+class ESuitsConfig(AppConfig):
+    name = 'samples'
--- esuits/urls.py
+++ esuits/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 
+app_name = 'esuits'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('signup/', views.SignupView.as_view(), name='signup'),
     path('home/', views.HomeView.as_view(), name='home'),
+
+    #esuits_utilsの動作確認用
+    path('samples/', include('esuits.samples.urls')),
 ]
