956d5626790c6ab5c5108c8eed4c15f8031339c6
yuasa <yuasalily1011@gmail.com>
2020-11-03 23:05:51 +0900

    Added change color sample

--- esuits/samples/urls.py
+++ esuits/samples/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from . import views
 from esuits.samples.newsapi_sample import views as newsapi_view
+from esuits.samples.change_color_sample import views as change_color_view
 
 
 app_name = 'samples'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('newsapi/', newsapi_view.newsapi_sample, name='newsapi_sample')
+    path('newsapi/', newsapi_view.newsapi_sample, name='newsapi_sample'),
+    path('changecolor/', change_color_view.change_color_sample, name="change_color_sample"),
 ]
