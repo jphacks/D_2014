be9535cd6d0b1fd0c33540d5e044d025c0337b1e
namihagi <niconico122112@gmail.com>
2020-11-07 03:30:42 +0900

    creating es_create

---
+++ static/esuits/js/es_create.js
@@ -0,0 +1,8 @@
+function getToday() {
+    var today = new Date();
+    today.setDate(today.getDate());
+    var yyyy = today.getFullYear();
+    var mm = ("0"+(today.getMonth()+1)).slice(-2);
+    var dd = ("0"+today.getDate()).slice(-2);
+    document.getElementById("deadline_date").value =yyyy+'-'+mm+'-'+dd;
+}
