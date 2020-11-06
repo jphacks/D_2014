e403b02e81f54482f68f8b54c92250ed6d07de95
namihagi <niconico122112@gmail.com>
2020-11-07 06:17:50 +0900

    modified es_create design

--- static/esuits/js/es_create.js
+++ static/esuits/js/es_create.js
@@ -2,7 +2,19 @@ function getToday() {
     var today = new Date();
     today.setDate(today.getDate());
     var yyyy = today.getFullYear();
-    var mm = ("0"+(today.getMonth()+1)).slice(-2);
-    var dd = ("0"+today.getDate()).slice(-2);
-    document.getElementById("deadline_date").value =yyyy+'-'+mm+'-'+dd;
+    var mm = ("0" + (today.getMonth() + 1)).slice(-2);
+    var dd = ("0" + today.getDate()).slice(-2);
+    document.getElementById("deadline_date").value = yyyy + '-' + mm + '-' + dd;
+}
+
+function addClassToSelect() {
+    // 質問の最後のform-groupをとるために，質問リストを取得
+    var parent = document.getElementById('question_list');
+    var lastChild = parent.lastElementChild;
+
+    // 最後の質問からtagのリストを取得
+    var select = lastChild.lastElementChild.lastElementChild;
+    select.setAttribute('class', 'selectpicker form-control');
+    select.setAttribute('data-selected-text-format', 'count > 3');
+    select.setAttribute('data-style', 'btn-dark');
 }
